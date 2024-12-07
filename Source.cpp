b4e5bd74e19825bc6405e54755d061b9e5650608
Pavel-Kirilenko <pavelkirilenko@mail.ru>
2024-12-07 16:30:04 +0400

    Исправить ошибку алгоритма, исключить утечки памяти, добавить функцию отображения содержания списка

--- Source.cpp
+++ Source.cpp
@@ -1,69 +1,79 @@
-﻿#include<mutex>
+#include <iostream>
+#include <mutex>
 
 struct Node
 {
-	int value;
-	Node* next;
-	std::mutex* node_mutex;
-	Node(int val) : value(val), next(nullptr), node_mutex(new std::mutex) {}   // конструктор структуры с инициализицией значения
-	~Node()                                                                    // деструктор структуры для предотвращения утечек памяти
-	{
-		delete node_mutex;
-	}
+    int value;
+    Node* next;
+    std::unique_ptr<std::mutex> node_mutex;
+    Node(int val) : value(val), next(nullptr), node_mutex(std::make_unique<std::mutex>()) {}   // конструктор структуры с инициализацией значения
+    ~Node() = default;                                                                         // деструктор структуры для предотвращения утечек памяти, unique_ptr сам освободит память
 };
 
 class FineGrainedQueue
 {
-	Node* head;
-	std::mutex* queue_mutex;
+    Node* head;
+    std::unique_ptr<std::mutex> queue_mutex;
 public:
-	FineGrainedQueue(Node* node) : head(node), queue_mutex(new std::mutex) {}  // конструктор класса с инициализацией структурой 'Node'
-	~FineGrainedQueue()                                                        // деструктор класса для предотвращения утечек памяти
-	{
-		delete queue_mutex;
-	}
-	void insertIntoMiddle(int value, int pos)
-	{
-		pos -= 2;                              // откорректировать номер позиции в алгоритме с учетом психологических особенностей человеческого восприятия
-		Node* newNode = new Node(value);       // определить вставляемый узел и инициализировать его заданным значением
-		queue_mutex->lock();                   // залочить мьютекс очереди целиком
-		Node* curNode = head;                  // присвоить вспомогательному текущему узлу значение 'head'
-		for (int i = 0; i < pos; i++)          // двигаться по списку до узла с заданным номером или до конца списка
-		{
-			if (!curNode->next) continue;      // выйти из цикла, если достигнут конец списка
-			curNode = curNode->next;           // перейти к следующему узлу
-		}
-		curNode->node_mutex->lock();           // залочить мьютекс текущего элемента
-		queue_mutex->unlock();                 // разлочить мьютекс очереди
-		newNode->node_mutex->lock();           // залочить мьютекс вставляемого элемента для дальнейших операций вставки
-		if (curNode->next)                     // если дальше по списку есть узел/узлы
-		{
-			newNode->next = curNode->next;     // вставить узел между узлами с инициализацией указателя на последующий узел
-			curNode->next = newNode;
+    FineGrainedQueue(Node* node) : head(node), queue_mutex(std::make_unique<std::mutex>()) {}  // конструктор класса с инициализацией структурой 'Node'
+    ~FineGrainedQueue() = default;                                                             // деструктор класса для предотвращения утечек памяти, unique_ptr сам освободит память
 
-		}
-		else                                   // иначе
-		{
-			curNode->next = newNode;           // вставить узел в конец списка без инициализации указателя на следующий элемент 
-		}
-		newNode->node_mutex->unlock();         // разлочить мьютекс вставленного узла
-		curNode->node_mutex->unlock();         // разлочить мьютекс предыдущего узла
-	};
+    void insertIntoMiddle(int value, int pos)
+    {
+        pos -= 2;                                                       // откорректировать номер позиции в алгоритме с учетом психологических особенностей человеческого восприятия
+        std::unique_ptr<Node> newNode = std::make_unique<Node>(value);  // определить вставляемый узел и инициализировать его заданным значением
+
+        {
+            std::lock_guard<std::mutex> lock(*queue_mutex);  // залочить мьютекс очереди целиком
+            Node* curNode = head;                            // присвоить вспомогательному текущему узлу значение 'head'
+            for (int i = 0; i < pos; i++)                    // двигаться по списку до узла с заданным номером или до конца списка
+            {
+                if (!curNode->next) break;                   // выйти из цикла, если достигнут конец списка
+                curNode = curNode->next;                     // перейти к следующему узлу
+            }
+            std::lock_guard<std::mutex> curNodeLock(*curNode->node_mutex);  // залочить мьютекс текущего элемента
+            std::lock_guard<std::mutex> newNodeLock(*newNode->node_mutex);  // залочить мьютекс вставляемого элемента для дальнейших операций вставки
+
+            if (curNode->next)                      // если дальше по списку есть узел/узлы
+            {
+                newNode->next = curNode->next;      // вставить узел между узлами с инициализацией указателя на последующий узел
+                curNode->next = newNode.release();  // передать владение указателем на новый узел текущему узлу
+            }
+            else                                    // иначе
+            {
+                curNode->next = newNode.release();  // вставить узел в конец списка без инициализации указателя на следующий элемент
+            }
+        }
+    }
+
+    void printQueue() const           // функция вывода содержимого списка для проверки работы алгоритма программы
+    {
+        Node* curNode = head;
+        while (curNode)
+        {
+            std::lock_guard<std::mutex> lock(*curNode->node_mutex);
+            std::cout << curNode->value << " ";
+            curNode = curNode->next;
+        }
+        std::cout << std::endl;
+    }
 };
 
 int main()
 {
-	Node* node = new Node(0);                  // определить узел с иннициализацией значения
-	FineGrainedQueue queue(node);              // создать ненулевой свисок
-	for (int i = 2; i < 6; i++)
-	{
-		queue.insertIntoMiddle(i, i);          // вставить узлы в список в различные позицие с различными значениями
-	}
+    Node* node = new Node(0);          // определить узел с инициализацией значения
+    FineGrainedQueue queue(node);      // создать ненулевой список
+    for (int i = 2; i < 6; i++)
+    {
+        queue.insertIntoMiddle(i, i);  // вставить узлы в список в различные позиции с различными значениями
+    }
 
-	queue.insertIntoMiddle(100, 2);
-	queue.insertIntoMiddle(200, 1000);
+    queue.insertIntoMiddle(100, 2);
+    queue.insertIntoMiddle(200, 1000);
 
-	return 0;                                  // рекомендуемая точка останова для проверки работы функции 'insertIntoMiddle'
+    queue.printQueue();                // вывести содержимое очереди
+
+    return 0;  
 }
 
 
